2591732f13d4313fea41900726cdea39afd567f5
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-20 22:50:04 +0100

    adding the base model file

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" base model """
+import datetime as dt
+import uuid
+
+
+class BaseModel:
+    """ base model """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime(now)
+        self.updated_at = datetime(now)
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime(now)
+
+    def to_dict(self):
+        new_dict = dict(self.__dict__)
+        new_dict["created_at"] = self.__dict__["created_at"].isoformat()
+        new_dict["updated_at"] = self.__dict__["updated_at"].isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
