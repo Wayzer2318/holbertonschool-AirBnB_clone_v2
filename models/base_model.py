8f01cd2931fe0a1d723b7cbcc16c6caee9eeb0cb
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-20 22:53:48 +0100

    fixed syntax error

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,17 @@ import uuid
 
 class BaseModel:
     """ base model """
+
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime(now)
-        self.updated_at = datetime(now)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime(now)
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         new_dict = dict(self.__dict__)
