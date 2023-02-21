c2dbcfedd63ba9d69f1c91239cb52778760723bd
snavy <1337snavy@gmail.com>
2023-02-21 18:54:10 +0100

    make tests visible to unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
+import unittest
+
 from models.base_model import BaseModel
 
 
-def test_id_is_str():
-    model = BaseModel()
-    assert str(type(model.id)) == "<class 'str'>"
+class Test_Base(unittest.TestCase):
+    def test_id_is_str(self):
+        model = BaseModel()
+        self.assertEqual(str(type(model.id)), "<class 'str'>")
 
+    def test_created_is_dtobj(self):
+        model = BaseModel()
+        self.assertEqual(str(type(model.created_at)), "<class 'datetime.datetime'>")
 
-def test_created_is_dtobj():
-    model = BaseModel()
-    assert str(type(model.created_at)) == "<class 'datetime.datetime'>"
-
-def test_updated_is_dtobj():
-    model = BaseModel()
-    assert str(type(model.updated_at)) == "<class 'datetime.datetime'>"
+    def test_updated_is_dtobj(self):
+        model = BaseModel()
+        self.assertEqual(str(type(model.updated_at)), "<class 'datetime.datetime'>")
