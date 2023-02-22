d26e10954b258bcadb8e9d9b5e327f9773b3c72a
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-22 11:15:39 +0100

    adding file storage

---
+++ engine/file_storage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" engine storage """
+from models.base_model import BaseModel
+import json
+
+
+class FileStorage:
+    """ file storage """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        self.__objects[type(obj).__name__ + "." + obj.id] = obj
+
+    def save(self):
+        ndict = {}
+        for key, value in FileStorage.__objects.items():
+            ndict[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dumps(ndict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, "r",
+                      encoding='utf-8') as read_file:
+                type(self).__objects = json.load(read_file)
+            for key, value in type(self).__objects.items():
+                obj = eval(type(self).__objects[key]['__class__'])(**value)
+                type(self).__objects[key] = obj
+
+        except FileNotFoundError:
+            pass
