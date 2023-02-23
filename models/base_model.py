a177c56dd2d996d58c72ce9bdf8d8123e3cd2f2c
snavy <1337snavy@gmail.com>
2023-02-23 17:20:23 +0000

    refactor base model

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         if kwargs:
-            created_at = datetime.datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            updated_at = datetime.datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            created_at = datetime.datetime.strptime(
+                kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            updated_at = datetime.datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
             kwargs["created_at"] = created_at
             kwargs["updated_at"] = updated_at
 
@@ -23,8 +25,13 @@ class BaseModel:
             self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        r = [
+            '[%s]' % (self.__class__.__name__),
+            '(%s)' % (self.id),
+            '%s' % (self.__dict__),
+        ]
+
+        return ' '.join(r)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
@@ -32,8 +39,8 @@ class BaseModel:
     def to_dict(self):
         new_dict = dict(self.__dict__)
 
-        new_dict["created_at"] = self.__dict__["created_at"].isoformat()
-        new_dict["updated_at"] = self.__dict__["updated_at"].isoformat()
         new_dict["__class__"] = self.__class__.__name__
-        
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+
         return new_dict
