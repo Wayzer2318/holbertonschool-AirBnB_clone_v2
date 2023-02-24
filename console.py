4a7349f11b5bebafca3cc91726cd99a573a3da09
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-24 03:39:23 +0100

    adding create function

--- console.py
+++ console.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 """ airbnb clone console program """
 import cmd
+import models
+from models.user import User
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ HBNB command class """
     prompt = '(hbnb) '
+    class_dict = {"BaseModel": BaseModel, "User": User}
+
+    def do_create(self, args):
+        """ create instance """
+        if not args:
+            print("** class name messing **")
+        else:
+            if args in HBNBCommand.class_dict.keys():
+                new_instance = HBNBCommand.class_dict[args]()
+                models.storage.save()
+                print(new_instance.id)
+            else:
+                print("** class doesn't exist **")
 
     def emptyline(self):
         """ handle empty line """
