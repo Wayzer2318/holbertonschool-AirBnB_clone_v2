0828ddeac9ec72d956978940ee08d56b01a9d686
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-12-19 14:42:31 +0100

    adding documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""imortation flask"""
+"""
+imortation flask
+"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
+    """
+    home
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
