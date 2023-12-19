cdcd6984e16542d5dc3d35ea15054cbcf0188de4
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-12-19 14:53:50 +0100

    fix pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,11 +9,12 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
+def index():
     """
     home
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='5000')
